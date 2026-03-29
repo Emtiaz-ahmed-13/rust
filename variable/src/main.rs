95283cb0f64aec7e2734fc2f4b79ab9929fb7357
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-03-29 10:00:00 +0600

    Add variable folder

---
+++ variable/src/main.rs
@@ -0,0 +1,25 @@
+const PI:u8=10; //can be global..
+
+
+fn main() {
+    //let fn =2; //cannot use fn as a variable name because fn is a keyword in Rust
+    let age:i32=24;
+    println!("value of age is {age}");
+    // age=25; //cannot assign twice to immutable variable
+    // println!("value of age is {age}");
+    // const PI:u8=10;
+    println!("value of PI is {PI}");
+
+    // const THREE_HOURS_IN_SECONDS:u32=60*60*3;
+    // println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+
+    //i cannnot use age in this threehoursesceond because its let variable so how i can use? i can use by changeing let to const 
+
+    const AGE:u32=24;
+    println!("value of AGE is {AGE}");
+
+    const THREE_HOURS_IN_SECONDS:u32=60*60*3+AGE;
+    println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+
+
+}
