87e72d6b294ad2dbe0361a2419424f770c477828
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-03-30 10:00:00 +0600

    finish

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -21,5 +21,11 @@ fn main() {
     const THREE_HOURS_IN_SECONDS:u32=60*60*3+AGE;
     println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
 
+    const ONE_HOUR:u64=60*60;
+
+    let time: u64=ONE_HOUR*30;
+
+    println!("value of time is {time}");
+
 
 }
