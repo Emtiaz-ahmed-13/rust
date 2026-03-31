839645896f4185eda88e956e2e6c7b7b0622aaab
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-03-31 10:00:00 +0600

    done

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,31 +1,44 @@
-const PI:u8=10; //can be global..
+// const PI:u8=10; //can be global..
 
 
 fn main() {
     //let fn =2; //cannot use fn as a variable name because fn is a keyword in Rust
-    let age:i32=24;
-    println!("value of age is {age}");
+    // let age:i32=24;
+    // println!("value of age is {age}");
     // age=25; //cannot assign twice to immutable variable
     // println!("value of age is {age}");
     // const PI:u8=10;
-    println!("value of PI is {PI}");
+    // println!("value of PI is {PI}");
 
     // const THREE_HOURS_IN_SECONDS:u32=60*60*3;
     // println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
 
     //i cannnot use age in this threehoursesceond because its let variable so how i can use? i can use by changeing let to const 
 
-    const AGE:u32=24;
-    println!("value of AGE is {AGE}");
+    // const AGE:u32=24;
+    // println!("value of AGE is {AGE}");
 
-    const THREE_HOURS_IN_SECONDS:u32=60*60*3+AGE;
-    println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+    // const THREE_HOURS_IN_SECONDS:u32=60*60*3+AGE;
+    // println!("value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
 
-    const ONE_HOUR:u64=60*60;
+    // const ONE_HOUR:u64=60*60;
 
-    let time: u64=ONE_HOUR*30;
+    // let time: u64=ONE_HOUR*30;
 
-    println!("value of time is {time}");
+    // println!("value of time is {time}");
+
+    // let apples : i32=10;
+
+    // let apples : i32=20;         //showdoing....
+
+    // println!("apples {apples}");
+
+
+    let apples: i32=10;
+
+    let apples : bool =true;
+
+    println!("apples {apples}");
 
 
 }
